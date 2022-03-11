daba0fb1e61f71225cc2993a79804d2c89a362c0
Kerymster <kerimyilmaz5270@gmail.com>
2022-03-11 16:10:41 +0300

    test

--- src/projects/projectCard/ProjectCard.js
+++ src/projects/projectCard/ProjectCard.js
@@ -7,7 +7,7 @@ const ProjectCard = () => {
       <div className="p-browser">
         <div className="p-circle"></div>
         <div className="p-circle"></div>
-        <div className="p-circle">test</div>
+        <div className="p-circle"></div>
       </div>
       <a href="https://github.com/Kerymster" target="_blank" rel="noreferrer">
         <img src="" alt="project-img" className="p-image" />
