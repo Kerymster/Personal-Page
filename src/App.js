fac57753696e9d65e96751cdc7f47ce4bf776fbe
Kerymster <kerimyilmaz5270@gmail.com>
2022-03-19 20:25:23 +0300

    first commit of contact component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Intro from "./Intro/Intro";
 import About from "./about/About";
 import ProjectList from "./projects/projectList/ProjectList";
+import Contact from "./contact/Contact";
+import "./App.css";
 function App() {
   return (
     <div className="App">
       <Intro />
       <About />
       <ProjectList />
+      <Contact />
     </div>
   );
 }
